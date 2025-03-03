1ae0054be198c9c465063d5939fe04b68bd09fc4
HuangFeiPeng <1094521084@qq.com>
2025-03-03 14:28:10 +0800

    init:初始化该项目文件.

---
+++ App.vue
@@ -0,0 +1,45 @@
+<script>
+  import EMClient from "@/utils/WebIM.js";
+  // 引入 EMPushUniPlugin 推送插件
+  // #ifdef APP-PLUS
+  const EMPushUniPlugin = uni.requireNativePlugin("EMPushUniPlugin");
+  // #endif
+  // 配置推送插件
+  const initPushOptions = () => {
+    const pushOption = {
+      // @ts-ignore
+      emPush: EMPushUniPlugin,
+      // 配置需要推送的证书名称
+      config: {
+        MICertificateName: "2882303761520334485", // 小米推送证书名称
+        // OPPOCertificateName: "xxxxxx", // oppo 推送证书名称
+        HMSCertificateName: "111809475", // 华为推送证书名称
+        // VIVOCertificateName: "xxxxxx", // vivo 推送证书名称
+        // HONORCertificateName: "xxxxxx", // 荣耀推送证书名称
+        // MEIZUCertificateName: "xxxxxx", // 魅族推送证书名称
+        // APNsCertificateName: "xxxxxx", // APNs推送证书名称
+      },
+    };
+    // 调用 IM SDK 方法，注册推送插件
+    EMClient.usePlugin(pushOption, "push");
+  };
+  export default {
+    // 在 uniapp onLaunch 事件中初始化推送插件
+    onLaunch: function () {
+      // #ifdef APP-PLUS
+      EMPushUniPlugin.initPushModule();
+      initPushOptions();
+      // #endif
+    },
+    onShow: function () {
+      console.log("App Show");
+    },
+    onHide: function () {
+      console.log("App Hide");
+    },
+  };
+</script>
+
+<style>
+  /*每个页面公共css */
+</style>
---
+++ utils/WebIM.js
@@ -0,0 +1,11 @@
+import EM_SDK from 'easemob-websdk/uniApp/Easemob-chat';
+const deviceInfo = uni.getDeviceInfo();
+console.log('deviceInfo', deviceInfo);
+let EMClient = new EM_SDK.connection({
+  appKey: 'easemob#easeim',
+  apiUrl: 'https://a1.easemob.com',
+  url: 'wss://im-api-wechat.easemob.com/websocket',
+  deviceId: deviceInfo.deviceId,
+});
+
+export default EMClient;
