0a3094af481d6eaf73fff2ac03aef6a9c85ed5ed
HuangFeiPeng <1094521084@qq.com>
2025-09-09 15:58:37 +0800

    feat:增加小米推送说明文档确保已经验证通小米推送.

--- utils/WebIM.js
+++ utils/WebIM.js
@@ -2,7 +2,7 @@ import EM_SDK from 'easemob-websdk/uniApp/Easemob-chat';
 const deviceInfo = uni.getDeviceInfo();
 console.log('deviceInfo', deviceInfo);
 let EMClient = new EM_SDK.connection({
-  appKey: 'easemob#easeim',
+  appKey: 'easemob-demo#support',
   apiUrl: 'https://a1.easemob.com',
   url: 'wss://im-api-wechat.easemob.com/websocket',
   deviceId: deviceInfo.deviceId,
